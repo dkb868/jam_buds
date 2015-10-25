aef81c14ce4b6977d9c2bee062206c571cc957a3
Slysmartie <slysmartie0@gmail.com>
2015-10-24 19:35:54 -0700

    CreateJamActivity and CreateJamActivityFragment do nothing (my additions do nothing) fragment_create_jam.xml has TextViews and EditTexts for the title, instruments, genres, location, and description strings.xml contains values for the message prompts for fragment_create_jam.xml (title, instruments, genres, location, and description)

--- app/src/main/java/com/example/mitrikyle/jambuds/CreateJamActivity.java
+++ app/src/main/java/com/example/mitrikyle/jambuds/CreateJamActivity.java
@@ -1,20 +1,38 @@
 package com.example.mitrikyle.jambuds;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class CreateJamActivity extends AppCompatActivity {
 
+    /**
+     * Trying to take message from CreateJamActivityFragment and display it
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_jam);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        // wtf setSupportActionBar(toolbar);
+
+        // Get  the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(CreateJamActivityFragment.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40); //magic numbers
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(R.layout.activity_create_jam);
     }
+
 
 }
